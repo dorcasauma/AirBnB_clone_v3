bf78ad82c8eafb5d60929dd2cf4d508865870763
DORCAS AUMA <dorcasauma89@gmail.com>
2024-05-28 12:59:45 +0300

    added documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,16 +4,38 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    return jsonify({"status": "OK"})
+    """
+    Retrieves the status of the API.
 
+    Returns:
+        JSON response with the status of the API.
+    Example:
+        {
+            "status": "OK"
+        }
+    """
+    return jsonify({"status": "OK"})
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """
-    Retrieves the number of each objects by type.
+    Retrieves the number of each object by type from the storage.
+    
+    Utilizes the storage.count() method to get the count of each object type.
+    
+    Returns:
+        JSON response containing the counts of each object type.
+    Example:
+        {
+            "Amenity": 10,
+            "City": 5,
+            "Place": 20,
+            "Review": 30,
+            "State": 7,
+            "User": 15
+        }
     """
     stats = {
         "Amenity": storage.count("Amenity"),
